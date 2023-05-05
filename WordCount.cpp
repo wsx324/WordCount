0d31a2f6112e13ffba7454e3b183197eb4b7665e
unknown <wusu324@foxmail.com>
2023-05-05 20:15:24 +0800

    作业

---
+++ WordCount.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s [-c | -w] filename\n", argv[0]);
+        return 1;
+    }
+    FILE *fp = fopen(argv[2], "r");
+    if (fp == NULL) {
+        printf("Can't open file %s!\n", argv[2]);
+        return 2;
+    }
+    
+    int count = 0;
+    if (strcmp(argv[1], "-c") == 0) {
+        while (fgetc(fp) != EOF) {
+            count++;
+        }
+        printf("×Ö·ûÊý: %d\n", count);
+    } else if (strcmp(argv[1], "-w") == 0) {
+        int state = 0; 
+        char c;
+        while ((c = fgetc(fp)) != EOF) {
+            if (c == ' ' || c == '\t' || c == '\n') {
+                state = 0; 
+            } else if (!state) {
+                state = 1; 
+                count++;
+            }
+        }
+        printf("µ¥´ÊÊý: %d\n", count);
+    }
+    
+    fclose(fp);
+    return 0;
+}
